5a15568f134fd917ffbfd36c0ad48ad0aa71a9d2
joelsalako <salakojoel.sunday@gmail.com>
2023-09-26 01:39:13 -0400

    Profile pic edited and portfolio5 pic changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css';
 import CTA from './CTA';
-import MYPIC from '../../assets/mypic.jpg';
+import MYPIC from '../../assets/mypic.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
